2101330644ca23ca8711cd633129836aef159f37
juan303 <gdf000@hotmail.com>
2018-06-04 01:09:46 +0200

    04_06

--- productos.php
+++ productos.php
@@ -20,41 +20,57 @@ include "librerias/consultas_bd.php";
 
             <div class="container">
                 <div class="row">
-                    <div class="col-12 text-center">
-                        <h1>Mis Productos</h1>
+                    <div class="col-12">
+                        <?php 
+                            $titulo = "Ultimos productos añadidos";
+                            if(isset($_GET['categoria'])){
+                                $titulo = nombre_categoria($conexion, $_GET['categoria']);
+                            }
+                        ?>
+                        <h1><?=$titulo;?></h1>
                     </div>
                 </div>
                 <div class="row">
+                    <div class="col-12">
+                    <nav class="navbar navbar-expand-sm bg-light navbar-light">
+                        <ul class="navbar-nav">
+                            <?php 
+                                $categorias = list_categorias($conexion);
+                                while($categoria = mysqli_fetch_array($categorias)){ 
+                                    $active = "";
+                                    if(isset($_GET['categoria']) && $_GET['categoria'] == $categoria['id']){
+                                        $active = 'active';
+                                    }
+                            ?>
+                                <li class="nav-item <?=$active;?>">
+                                <a class="nav-link" href="?categoria=<?=$categoria['id'];?>"><?=$categoria['nombre'];?></a>
+                                </li>
+                            <?php } ?>
+                        </ul>
+                        </nav>
+                    </div>
+                   
+                </div>
+                <div class="row">
+                <?php 
+                    $id_categoria = "todos";
+                    if(isset($_GET['categoria'])){
+                        $id_categoria = $_GET['categoria'];
+                    }
+                    $productos = list_productos($conexion, 'fecha', $id_categoria);
+                    while($producto = mysqli_fetch_array($productos)){ 
+                ?>
                     <div class="col-sm-12 col-md-6 col-lg-4">
                         <div class="card">
-                            <img class="card-img-top img-fluid" src="imagenes/productos/1.jpg" alt="Card image cap">
+                            <img class="card-img-top img-fluid" src="<?=$producto['imagen'];?>" alt="Card image cap">
                             <div class="card-body">
-                                <h5 class="card-title">Mando Arcade 1</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <h5 class="card-title"><?=$producto['nombre'];?></h5>
+                                <p class="card-text"><?=$producto['descripcion_corta'];?></p>
                                 <a href="#" class="btn btn-primary">Detalles...</a>
                             </div>
                         </div>
                     </div>
-                    <div class="col-sm-12 col-md-6 col-lg-4">
-                        <div class="card">
-                            <img class="card-img-top img-fluid" src="imagenes/productos/2.jpg" alt="Card image cap">
-                            <div class="card-body">
-                                <h5 class="card-title">Mando Arcade 1</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Detalles...</a>
-                            </div>
-                        </div> 
-                    </div>
-                        <div class="col-sm-12 col-md-6 col-lg-4">
-                        <div class="card">
-                            <img class="card-img-top img-fluid" src="imagenes/productos/3.jpg" alt="Card image cap">
-                            <div class="card-body">
-                                <h5 class="card-title">Mando Arcade 1</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Detalles...</a>
-                            </div>
-                        </div> 
-                    </div>
+                    <?php } ?>
                 </div>
             </div>
             <div class="container-fluid">
